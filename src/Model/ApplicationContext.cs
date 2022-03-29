d3be4047cf5d27652e9110c966371e33a577da7a
Denis Makarenko <denis-makarenko@outlook.com>
2022-03-29 08:29:21 +0300

    Fix after testing

--- src/Model/ApplicationContext.cs
+++ src/Model/ApplicationContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Model.Enums;
 using Model.Models.Entities;
 
 namespace Model;
@@ -19,7 +18,6 @@ public class ApplicationContext : DbContext
         modelBuilder.Entity<User>().HasData(new User { Id = Guid.Parse("a96fe69e-cc4c-45a8-a05d-dc498368a766"), UserName = "user", PasswordHash = "04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb"});
         modelBuilder.Entity<UserRole>().HasData(new UserRole{Id = Guid.NewGuid(), UserId = Guid.Parse("02387541-4280-4d14-86d9-9386310de6ce"), RoleId = Guid.Parse("6b22b9a0-bc42-4b88-96ef-a0c4539d6dac")});
         modelBuilder.Entity<UserRole>().HasData(new UserRole{Id = Guid.NewGuid(),  UserId = Guid.Parse("a96fe69e-cc4c-45a8-a05d-dc498368a766"), RoleId = Guid.Parse("2e1d11bf-e1f6-4eb8-9042-5fd6a4bbf6e2")});
-        modelBuilder.Entity<Key>().HasData(new Key{Id = Guid.Parse("0b209a41-72a5-4572-89b6-3ffa7b9f994f"), Type = KeyTypeEnum.Tag});
     }
 
     public DbSet<User> Users { get; set; }
--- src/Model/Models/Entities/OpeningHistory.cs
+++ src/Model/Models/Entities/OpeningHistory.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Model.Models.Entities;
 
@@ -6,6 +7,9 @@ public class OpeningHistory : EntityBase
 {
     [Required]
     public Guid LockId { get; set; }
+    
+    [ForeignKey(nameof(LockId))]
+    public virtual Lock Lock { get; set; }
     
     [Required]
     public Guid AccessId { get; set; }
